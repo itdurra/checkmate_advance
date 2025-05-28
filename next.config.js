614b1951a5ac6807eeae79c420afe45c81b5fc21
Yam <itdurra@outlook.com>
2025-05-28 16:43:07 -0700

    changed cross origin policy

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
     domains: ['storage.ko-fi.com'],
   },
-    async headers() {
-        return [
-          {
-            source: "/:path*",
-            headers: [
-              { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-              { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-            ],
-          },
-        ];
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Cross-Origin-Embedder-Policy', value: 'credentialless' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+        ],
       },
+    ];
+  },
 };
 
 module.exports = nextConfig;
